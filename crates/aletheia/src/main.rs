86e2b94b0aee43fe9cb5e6db3d33c80ca9df66a6
odyslam <odyslam@gmail.com>
2021-07-31 23:44:37 +0300

    aletheia package skeleton

--- crates/aletheia/src/main.rs
+++ crates/aletheia/src/main.rs
@@ -1,32 +1,3 @@
-use csv::Writer;
-use scraper::Html;
-use scraper::Selector;
-use select::document::Document;
-use select::predicate::{Attr, Class, Name};
-use std::fs::OpenOptions;
-
-async fn test() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let url = format!("https://www.amazon.com/Gossner-Foods-Milk-32oz-Pack/dp/B00U4D276I/ref=sr_1_73?_encoding=UTF8&c=ts&dchild=1&keywords=Dairy+Milks&qid=1625493404&s=grocery&sr=1-73&ts_id=6520438011");
-    let response = reqwest::get(&url).await?.text().await?;
-    // println!("body: {:?}", &response);
-    let document = Html::parse_document(&response);
-    let selector = Selector::parse("#priceblock_ourprice").unwrap();
-    for element in document.select(&selector) {
-        println!("element: {:?}", element.inner_html())
-    }
-    Ok(())
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut handles: std::vec::Vec<_> = Vec::new();
-    let job = tokio::spawn(async move { test().await });
-    handles.push(job);
-
-    let mut results = Vec::new();
-    for job in handles {
-        results.push(job.await);
-    }
-
-    Ok(())
+fn main() {
+    println!("Hello world!");
 }
--- crates/hermes/src/main.rs
+++ crates/hermes/src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use csv::Writer;
+use scraper::Html;
+use scraper::Selector;
+use select::document::Document;
+use select::predicate::{Attr, Class, Name};
+use std::fs::OpenOptions;
+use tokio;
+use reqwest;
+use url::Url;
+
+struct Parser {
+    client: reqwest::Client
+    
+}
+
+impl Parser {
+    async fn init_parser(&mut self) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        self.client = reqwest::Client::new();
+        Ok(())
+    }
+    async fn scrape_website(&self, url: &str, selector: &str) ->  Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        let url = Url::parse(&url)?;
+        let response = self.client.get(url).send().await?.text().await?;
+        let document = Html::parse_document(&response);
+        let selector = Selector::parse(&selector).unwrap();
+        for element in document.select(&selector) {
+            println!("element: {:?}", element.inner_html())
+        }
+        Ok(())
+            
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut handles: std::vec::Vec<_> = Vec::new();
+    let client = reqwest::Client::new();
+    let job = tokio::spawn(async move { scrape_website().await });
+    handles.push(job);
+
+    let mut results = Vec::new();
+    for job in handles {
+        results.push(job.await);
+    }
+
+    Ok(())
 }
