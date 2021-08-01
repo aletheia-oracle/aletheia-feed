d79f197b37e26ab4a6b4588654b4a5551a450d08
odyslam <odyslam@gmail.com>
2021-08-01 13:51:53 +0300

    hermes, aletheia | fix hermes package name and resolution

--- crates/hermes/src/lib.rs
+++ crates/hermes/src/lib.rs
@@ -4,21 +4,18 @@ use scraper::Selector;
 use select::document::Document;
 use select::predicate::{Attr, Class, Name};
 use std::fs::OpenOptions;
-use tokio;
 use reqwest;
 use url::Url;
 
-struct Parser {
+pub struct Parser {
     client: reqwest::Client
-    
 }
 
 impl Parser {
-    async fn init_parser(&mut self) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    pub fn init_parser(&mut self){
         self.client = reqwest::Client::new();
-        Ok(())
     }
-    async fn scrape_website(&self, url: &str, selector: &str) ->  Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    pub async fn scrape_website(&self, url: &str, selector: &str) ->  Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let url = Url::parse(&url)?;
         let response = self.client.get(url).send().await?.text().await?;
         let document = Html::parse_document(&response);
@@ -29,19 +26,4 @@ impl Parser {
         Ok(())
             
     }
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut handles: std::vec::Vec<_> = Vec::new();
-    let client = reqwest::Client::new();
-    let job = tokio::spawn(async move { scrape_website().await });
-    handles.push(job);
-
-    let mut results = Vec::new();
-    for job in handles {
-        results.push(job.await);
-    }
-
-    Ok(())
 }
